9a1adfb7f4e4a2ce8f5f44151857255810e1859b
sudha <sudha.sivaprakasam@gmail.com>
2024-06-13 11:37:41 +0000

    test

--- provider.tf
+++ provider.tf
@@ -7,11 +7,32 @@ terraform {
   }
 }
 
+
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
   #
   # delete_vm_but_do_not_delete_storage
-
+  subscription_id = var.subscription_id 
+  client_id =  var.client_id
+  client_secret =  var.client_secret
+  tenant_id =  var.tenant_id
 }
+
+variable "client_id" {
+  type = string 
+}
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+
